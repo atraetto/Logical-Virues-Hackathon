f10a8e717ad983835904a6edd2fc9fbf3bd594b9
Andrew <atraetto@gmail.com>
2020-12-04 08:47:35 -0500

    hacking away for cloud deployment

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,13 @@
-from flask import Flask
+# from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    app.config['FLASK_ENV'] = 'development'
-    app.config['DEBUG'] = False
-    app.config['TESTING'] = True
-    app.secret_key = 'super secret key'
+# def create_app():
+#     app = Flask(__name__)
+#     app.config['FLASK_ENV'] = 'development'
+#     app.config['DEBUG'] = False
+#     app.config['TESTING'] = True
+#     app.secret_key = 'super secret key'
 
-    with app.app_context():
-        from .routes import uiRoutes
+#     with app.app_context():
+#         from .routes import uiRoutes
 
-        return app
+#         return app
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
-from application import create_app
+# from application import create_app
 
-application = create_app()
+# application = create_app()
 
-if __name__ == "__main__":
-    application.run()
+# if __name__ == "__main__":
+#     application.run()
+
+from app.main import app
+
+if __name__ == '__main__':
+    app.run()
