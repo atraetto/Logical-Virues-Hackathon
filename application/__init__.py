b46c7d72dc5d5313c4c01e9dfe1ea2978efe1a74
Andrew <atraetto@gmail.com>
2020-12-03 19:34:21 -0500

    Results page with door images based on url param

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ def create_app():
     app.config['FLASK_ENV'] = 'development'
     app.config['DEBUG'] = False
     app.config['TESTING'] = True
+    app.secret_key = 'super secret key'
 
     with app.app_context():
         from .routes import uiRoutes
--- application/routes/uiRoutes.py
+++ application/routes/uiRoutes.py
@@ -1,5 +1,18 @@
-from flask import current_app as app, render_template
+from flask import current_app as app, render_template, url_for
+
+
 @app.route('/')
-def hello():
-    # return "Hello World!"
+def index():
     return render_template('index.html')
+
+@app.route('/result/<maskIndicator>')
+def result(maskIndicator):
+
+    if maskIndicator == '1':
+        print("Mask detected, door unlocked")
+        door_filename = url_for('static', filename='images/door-open.jpg')
+    else:
+        print("No mask detected, door locked")
+        door_filename = url_for('static', filename='images/door-closed.jpg')
+
+    return render_template('result.html', door_image=door_filename)
