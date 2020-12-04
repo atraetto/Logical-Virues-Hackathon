1ca90302e288f2edd5dd01e434c5bf4fc03d187e
Andrew <atraetto@gmail.com>
2020-12-03 20:47:03 -0500

    small tweak

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=False)
+    application=app = Flask(__name__)
     app.config['FLASK_ENV'] = 'development'
     app.config['DEBUG'] = False
     app.config['TESTING'] = True
